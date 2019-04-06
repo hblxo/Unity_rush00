bf8dd7c159525d2b5e0011d5da34836b5725605e
Nicolas IRAGNE <niragne@e1r11p6.42.fr>
2019-04-06 22:28:15 +0200

    added player script and weapon scripts

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon {
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.Projectile = Resources.Load("BulletPistolPrefab") as GameObject;
+		this.FireRate = 0.5f;
+		Debug.Log(this.Projectile);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+	public GameObject Projectile;
+	public float FireRate;
+	public float Range;
+	public float BulletSpeed;
+	public int Ammo;
+	public float NextShot;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		NextShot = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Shoot(Transform source, Vector3 target)
+	{
+		if (Time.time > NextShot)
+		{
+			var clone = Instantiate(Projectile, transform.position, transform.rotation);
+			clone.GetComponent<Bullet>().Target = target;
+			NextShot = Time.time + FireRate;
+		}
+		Debug.Log("NextShot, Time " + NextShot + " " + Time.time);
+	}
+}
