246e55cabfbd7e53351d09ff579241afae3a9c73
Leo BOUCHET <hbouchet@e1r12p5.42.fr>
2019-04-07 04:29:58 +0200

    Add Ammo HUD Management

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Gm;
+	public int PlayerAmmo;
+	private Weapon _weapon;
+	public GameObject Player;
+	
+	//Singleton basique  : Voir unity design patterns sur google.
+	void Awake () {
+		if (Gm == null)
+			Gm = this;
+	}
+	
+	// Use this for initialization
+	void Start ()
+	{
+		PlayerAmmo = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		UpdateAmmo();
+	}
+
+	private void UpdateAmmo()
+	{
+		_weapon = Player.GetComponent<Weapon>();
+		if (_weapon)
+			PlayerAmmo = _weapon.Ammo;
+		else
+			PlayerAmmo = 0;
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	public GameManager GM;
+	public Text Ammo;
+	
+	// Use this for initialization
+	void Start () {
+		SetProperties();
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		SetProperties();
+	}
+	
+	public void SetProperties()
+	{
+		Ammo.text = GM.PlayerAmmo.ToString();
+	}
+}
